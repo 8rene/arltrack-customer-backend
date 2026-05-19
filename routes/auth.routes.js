fcbcf640cd7f2966e338f2e045c916bf8be1d653
8rene <irene.borja@dorsu.edu.ph>
2026-05-19 17:49:06 +0800

    fix: add google login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { signup, checkAvailability } = require("../controllers/auth/signup.controller");
 const { login }   = require("../controllers/auth/login.controller");
 const { sendOTP } = require("../controllers/auth/otp.controller");
+const { googleLogin } = require("../controllers/auth/google.controller"); // ← add
 const validateSignup = require("../middlewares/signup.middleware");
 const validateLogin  = require("../middlewares/login.middleware");
 
@@ -12,5 +13,6 @@ router.get("/check-availability", checkAvailability);
 router.post("/signup",            validateSignup, signup);
 router.post("/login",             validateLogin,  login);
 router.post("/send-otp",          sendOTP);
+router.post("/google",            googleLogin); // ← add
 
 module.exports = router;
