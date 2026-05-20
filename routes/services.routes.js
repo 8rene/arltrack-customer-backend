d8681eaa3a62b8972934a2df82e9acb92a4a828f
8rene <irene.borja@dorsu.edu.ph>
2026-05-20 08:36:15 +0800

    services routes

--- routes/services.routes.js
+++ routes/services.routes.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const router  = express.Router();
+const express      = require("express");
+const router       = express.Router();
+const { verifyToken } = require("../middlewares/auth.middleware");
 
 const { getServices, getServiceTypes, getCarBookings } = require("../controllers/services/services.controller");
 
-router.get("/",                      getServices);
-router.get("/types",                 getServiceTypes);
-router.get("/car-bookings/:carID",   getCarBookings);
+router.get("/",                                  getServices);
+router.get("/types",                             getServiceTypes);
+router.get("/car-bookings/:carID", verifyToken,  getCarBookings);
 
 module.exports = router;
